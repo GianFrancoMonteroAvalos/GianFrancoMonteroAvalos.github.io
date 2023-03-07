3141d36cfa4246ce5270220d195934085714efbd
GianFrancoMonteroAvalos <124310539+GianFrancoMonteroAvalos@users.noreply.github.com>
2023-03-06 21:16:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const asunto = document.getElementById("asunto")
+const mensaje = document.getElementById("mensaje")
+
+
+
